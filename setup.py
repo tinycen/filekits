f99944d2a7e5a146750e1b0069a08d858fbc767d
tinycen <sky_ruocen@qq.com>
2026-05-25 16:22:01 +0800

    chore: bump version to v0.2.23
    update version tag in create_tag.sh and setup.py for new release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except FileNotFoundError:
 
 setup(
     name='filekits',
-    version='0.2.22',
+    version='0.2.23',
     packages=find_packages(),
     install_requires=install_requires,
     extras_require={
